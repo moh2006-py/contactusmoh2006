8fdafbff66f60d57d57b3d9e234c92072dd423cf
moh2006-py <74707334+moh2006-py@users.noreply.github.com>
2021-02-25 15:10:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
       <div class="contact-info">
         <div><i class="fas fa-map-marker-alt"></i>Address, City, Country</div>
         <div><i class="fas fa-envelope"></i>contact@email.com</div>
-        <div><i class="fas fa-phone"></i>+00 0000 000 000</div>
+        <div><i class="fas fa-phone"></i>+964 771 3218 239</div>
         <div><i class="fas fa-clock"></i>Mon - Fri 8:00 AM to 5:00 PM</div>
       </div>
       <div class="contact-form">
